eacd118275206abc8feb09716ed9ab7d082cf6f2
jnicolasdz <jndiazs@udistrital.edu.co>
2025-02-11 16:02:46 -0500

    setting up dev environment

---
+++ backend-java/src/main/java/com/Advanced_Programming_Project_Drive_UDJC/backend_java/BackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.Advanced_Programming_Project_Drive_UDJC.backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaApplication.class, args);
+	}
+
+}
